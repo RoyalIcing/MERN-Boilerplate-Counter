1607ec2ed05413c3bd5c92d33e8ef80b5b5eeb69
Patrick Smith <patrick@burntcaramel.com>
2016-12-22 11:59:13 +1100

    Allow CORS from any origin

--- api/app.js
+++ api/app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: '*'
 }));
 
 app.use('/', index);
