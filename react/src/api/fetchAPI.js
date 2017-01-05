afc33359d9356e54d2ceba8ea13101a697ee2b8b
Patrick Smith <patrick@burntcaramel.com>
2017-01-05 15:24:27 +1100

    React: Add auth API

--- react/src/api/fetchAPI.js
+++ react/src/api/fetchAPI.js
@@ -2,3 +2,23 @@ export default function fetchAPI(path, options) {
     return fetch(process.env.REACT_APP_API_URL + path, options)
       .then(res => res.json())
 }
+
+export function postAPI(path, bodyJSON) {
+  return fetchAPI(path, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(bodyJSON)
+  })
+}
+
+export function patchAPI(path, bodyJSON) {
+  return fetchAPI(path, {
+      method: 'PATCH',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(bodyJSON)
+  })
+}
